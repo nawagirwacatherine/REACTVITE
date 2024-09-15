95904554bd94c505d63fe9edfbfc8c70f1fdf479
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-09-15 06:50:46 +0200

    added useState hook

--- vreact-app/src/App.jsx
+++ vreact-app/src/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react"
 import "./style.css"
-
 export default function App(){
+  const [newItem, setNewItem] = useState("item 1")
   return(
     <> <form className="new-item-form">
     <div className="form-row">
       <label htmlFor="item">New Item</label>
-      <input type="text" id="item" />
+      <input value={newItem} 
+      onChange={e => setNewItem(e.target.value)}
+      type="text" 
+      id="item" />
     </div>
     <button className="btn">Add</button>
-
 
   </form>
   <h1 className="header">Todo List</h1>
